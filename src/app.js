ef7ee291b5b486aba38d54bb3f0c8fb12262e58f
Alexandre Junior <jasj_jr@hotmail.com>
2020-05-13 08:16:02 -0300

    Update, delete and like repositories

--- src/app.js
+++ src/app.js
@@ -26,15 +26,42 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const i = repositories.findIndex(repo => repo.id === id)
+
+  if (i === -1) return response.status(400).json({ error: 'Repository does no exist' })
+
+  repositories[i].title = title || repositories[i].title
+  repositories[i].url = url || repositories[i].url
+  repositories[i].techs = techs || repositories[i].techs
+
+  return response.json(repositories[i])
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const i = repositories.findIndex(repo => repo.id === id)
+
+  if (i === -1) return response.status(400).json({ error: 'Repository does no exist' })
+
+  repositories.splice(i, 1)
+
+  return response.status(204).json()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const i = repositories.findIndex(repo => repo.id === id)
+
+  if (i === -1) return response.status(400).json({ error: 'Repository does no exist' })
+
+  repositories[i].likes += 1
+
+  return response.json(repositories[i])
 });
 
 module.exports = app;
